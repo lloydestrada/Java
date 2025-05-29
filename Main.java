f9970d77c2c57ab76dd55b5b4f2267f78f928b3e
lloydestrada <iamlloydestrada@gmail.com>
2025-05-29 14:45:48 +0800

    one

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args){
 
-        String[] fruit = {"Apple", "banana", "calamansi", "eggplant"};
+        String[] fruit = {"Apple", "banana", "calamansi", "eggplant","peach"};
 
         System.out.println(fruit[3]);
         System.out.println(fruit[2]);
         System.out.println(fruit[0]);
+        System.out.println(fruit[4]);
     }
 }
