830653cc8009256f505a1eefae573f8d9bf127e7
lloydestrada <iamlloydestrada@gmail.com>
2025-06-03 14:28:07 +0800

    arrays

--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        int[] numbers = {1, 2, 3, 6, 8, 9, 11,};
+        Scanner scanner = new Scanner(System.in);
+        //int[] numbers = {1, 2, 3, 6, 8, 9, 11,};
+        String [] cars = {"Honda", "Mitsubishi", "Ford", "Toyota", "BMW", "Subaru", "Suzuki"};
 
-        int target = 2;
-
+        String target;
         boolean isFound = false;
 
-        for(int i = 0; i < numbers.length; i++){
-            if(target == numbers[i]) {
+        System.out.print("Enter the Car brand you want to find: ");
+        target = scanner.nextLine();
+
+        for(int i = 0; i < cars.length; i++){
+            if(cars[i].equals(target)) {
                 System.out.println("Element found: " + i);
                 isFound = true;
                 break;
@@ -21,5 +25,7 @@ public class Main {
         if(!isFound){
             System.out.println("Nothing found");
         }
+
+        scanner.close();
     }
 }
