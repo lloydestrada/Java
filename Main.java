d537af2addb16264d2fd62b2ef24d7f7c4e99f68
lloydestrada <iamlloydestrada@gmail.com>
2025-06-06 19:48:50 +0800

    varags2

--- Main.java
+++ Main.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(add(1,2,3,4));
+        System.out.println(average());
 
     }
-    static int add(int... numbers){
 
-        int sum = 0;
+    static double average(double... numbers){
 
-        for(int number : numbers){
+        double sum = 0;
+
+        if(numbers.length == 0){
+            return 0;
+        }
+
+        for(double number : numbers){
             sum += number;
         }
-        return  sum;
+        return sum / numbers.length;
     }
 }
