e17da14eac761aa1f79a2d1fcc628013957f2088
lloydestrada <iamlloydestrada@gmail.com>
2025-06-09 00:06:41 +0800

    constructor

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
 
+        Car car1 = new Car("Honda", "Civic Type R", 2024, 4);
+        Car car2 = new Car("Toyota", "GR86", 2024, 4);
 
     }
 }
