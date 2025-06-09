8edf5d853f96c6e0fde98745cc6b2582e48d8c00
lloydestrada <iamlloydestrada@gmail.com>
2025-06-09 15:54:06 +0800

    major update

--- Constructor.java
+++
@@ -1,8 +0,0 @@
-public class Constructor {
-    public static void main(String[] args){
-
-        Car car1 = new Car("Honda", "Civic Type R", 2024, 4);
-        Car car2 = new Car("Toyota", "GR86", 2024, 4);
-
-    }
-}
