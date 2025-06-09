e834386c6210019758eec6d1263e340da5ea508e
lloydestrada <iamlloydestrada@gmail.com>
2025-06-09 15:59:14 +0800

    update

---
+++ Java_OOP/Encapsulation/Main.java
@@ -0,0 +1,8 @@
+package Java_OOP.Encapsulation;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
---
+++ Java_OOP/Polymorphism/Main.java
@@ -0,0 +1,8 @@
+package Java_OOP.Polymorphism;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
