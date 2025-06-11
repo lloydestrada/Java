60ea683de42e477cde5db83c8ce8cc4a49788b71
lloydestrada <iamlloydestrada@gmail.com>
2025-06-11 22:44:28 +0800

    comment

--- MethodOverriding/Main.java
+++ MethodOverriding/Main.java
@@ -3,6 +3,8 @@ package MethodOverriding;
 public class Main {
     public static void main(String[] args){
 
+
+        //method overriding = subclass provide its own method that is already defined.
         Dog dog = new Dog();
         Bird bird = new Bird();
         Fish fish = new Fish();
