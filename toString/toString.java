fdefba4fc13eb1314010a3a5cea90fec0ba61a54
lloydestrada <iamlloydestrada@gmail.com>
2025-06-11 23:35:23 +0800

    comment

--- toString/toString.java
+++ toString/toString.java
@@ -4,7 +4,8 @@ public class toString {
     public static void main(String[] args){
 
 
-        //.toString() = method inherited from the Object class
+        //.toString() = method inherited from the Object class.
+        //Basically, converts hash code into a meaningful details.
         Car car = new Car("Honda", "Cvic", 2024, "Dark Blue");
         Car car2 = new Car("Ford", "Mustang", 2025, "Blue" );
 
