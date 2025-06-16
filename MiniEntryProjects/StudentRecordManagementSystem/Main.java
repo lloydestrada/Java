68314e506453f9aa2588531f46a9d299e79ae4ce
lloydestrada <iamlloydestrada@gmail.com>
2025-06-16 23:56:19 +0800

    final assignment

--- MiniEntryProjects/StudentRecordManagementSystem/Main.java
+++ MiniEntryProjects/StudentRecordManagementSystem/Main.java
@@ -20,7 +20,8 @@ public class Main {
             System.out.println("1. Add New Student");
             System.out.println("2. View Student");
             System.out.println("3. Update Student Details");
-            System.out.println("4. Exit\n");
+            System.out.println("4. List All Students");
+            System.out.println("5. Exit\n");
             System.out.print("Enter your options: ");
             int choices = scanner.nextInt();
 
@@ -29,7 +30,8 @@ public class Main {
                 case 1 -> addStudent(scanner, students);
                 case 2 -> viewStudent(scanner, students);
                 case 3 -> updateStudent(scanner, students);
-                case 4 -> isRunning = false;
+                case 4 -> listStudents(students);
+                case 5 -> isRunning = false;
             }
         }
 
@@ -40,14 +42,26 @@ public class Main {
     }
     //Add Student
     public static void addStudent(Scanner scanner, ArrayList<Student> students){
-
+        boolean isDuplicate;
+        int roll;
         scanner.nextLine();
         System.out.print("Enter The Full Name: ");
         String name = scanner.nextLine();
 
-        System.out.print("Roll Number: ");
-        int roll = scanner.nextInt();
-        scanner.nextLine();
+        do{
+            isDuplicate = false;
+            System.out.print("Roll Number: ");
+            roll = scanner.nextInt();
+            scanner.nextLine();
+
+            for(Student student : students){
+                if(student.getRollNumber() == roll){
+                    isDuplicate = true;
+                    System.out.println("Roll Number already exist");
+                    break;
+                }
+            }
+        }while(isDuplicate);
 
         System.out.print("Department: ");
         String department = scanner.nextLine();
@@ -71,7 +85,8 @@ public class Main {
         for(Student student : students) {
             if (student.getRollNumber() == roll) {
                 isfound = true;
-                System.out.println("\n-----Details----");
+                System.out.println("\n------------------");
+                System.out.println("-----Details----");
                 System.out.println("------------------");
                 System.out.println("Name: " + student.getName());
                 System.out.println("Roll Number: " + student.getRollNumber());
@@ -97,11 +112,12 @@ public class Main {
                 isFound = true;
                 while(isFound){
 
-                    System.out.println("What do you want to update?");
+                    System.out.println("\nWhat do you want to update?");
                     System.out.println("1. Name");
                     System.out.println("2. Department");
                     System.out.println("3. GPA");
                     System.out.println("4. Back to menu");
+                    System.out.print("Enter your options: ");
                     int choice = scanner.nextInt();
 
                     switch (choice){
@@ -144,5 +160,23 @@ public class Main {
             System.out.println("Student not found");
         }
 
+    }
+
+    public static void listStudents(ArrayList<Student> students){
+
+        if(students.isEmpty()){
+            System.out.println("\nNo Student found");
+        }
+
+
+        System.out.println("\n----- ALL LISTED STUDENTS -----\n");
+        for(Student student : students){
+            System.out.println("---------------------------------");
+            System.out.println("Name: " + student.getName());
+            System.out.println("Roll Number : " + student.getRollNumber());
+            System.out.println("Department :" + student.getDepartment());
+            System.out.println("GPA: " + student.getGpa());
+            System.out.println(" ");
+        }
     }
 }
