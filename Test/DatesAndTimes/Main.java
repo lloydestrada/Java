ad5c167325dcb7c2cfecca8ce1cb4f9d79cabb28
lloydestrada <iamlloydestrada@gmail.com>
2025-06-26 18:54:43 +0800

    nice

--- Test/DatesAndTimes/Main.java
+++ Test/DatesAndTimes/Main.java
@@ -1,6 +1,10 @@
 package Test.DatesAndTimes;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
@@ -8,7 +12,19 @@ public class Main {
 
 
         LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        LocalDateTime dateTime = LocalDateTime.now();
 
         System.out.println(date);
+        System.out.println(time);
+        System.out.println(dateTime);
+
+        Instant instant = Instant.now();
+
+        System.out.println(instant);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+        String newDateTime = dateTime.format(formatter);
+        System.out.println(newDateTime);
     }
 }
--- out/production/Java_BroCode/README.md
+++ out/production/Java_BroCode/README.md
@@ -1,7 +1,7 @@
 # Java Learning Journey
 
 This repository is a collection of mini projects, hands-on practice files, and core concept experiments written in Java. 
-I'm currently learning Java and using this space to apply what I study — from basic syntax to object-oriented programming (OOP) and beyond.
+I'm currently learning Java and using this space to apply what I study, from basic syntax to object-oriented programming (OOP) and beyond.
 
 I'm self-learning through various resources like the internet, YouTube tutorials, coding forums, and documentation. 
 Exploring different Java topics, building small utilities, testing concepts, and improving step by step.
