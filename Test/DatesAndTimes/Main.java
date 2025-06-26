da6770d3456c0846c0e10d7ec06015f177f1f05a
lloydestrada <iamlloydestrada@gmail.com>
2025-06-26 18:46:21 +0800

    date

---
+++ Test/DatesAndTimes/Main.java
@@ -0,0 +1,14 @@
+package Test.DatesAndTimes;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+        LocalDate date = LocalDate.now();
+
+        System.out.println(date);
+    }
+}
