60191dd308da460d6dfd3f5671ec310cd6f72582
lloydestrada <iamlloydestrada@gmail.com>
2025-06-27 20:02:15 +0800

    .

---
+++ Test/AnonymousClass/Dog.java
@@ -0,0 +1,9 @@
+package Test.AnonymousClass;
+
+public class Dog {
+
+    void speak(){
+
+        System.out.println("The dog barks!");
+    }
+}
---
+++ Test/AnonymousClass/Main.java
@@ -0,0 +1,15 @@
+package Test.AnonymousClass;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Anonymous class = a class does not have a name, and cannot be reused.
+
+
+        Dog dog = new Dog();
+
+        dog.speak();
+
+    }
+}
