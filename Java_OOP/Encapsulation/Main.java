895ab7511ed52617ca276a5b4b6b2f7a5170d4b3
lloydestrada <iamlloydestrada@gmail.com>
2025-07-02 23:09:09 +0800

    clean

--- Java_OOP/Encapsulation/Main.java
+++ Java_OOP/Encapsulation/Main.java
@@ -4,26 +4,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a date (dd/MM/yyyy): ");
-        String inputDate = scanner.nextLine();
+public class Main{
 
-        try {
-            // Parse the input date string to a Date object
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date date = dateFormat.parse(inputDate);
+    public  static void main(String[] args){
 
-            // Format to get day name
-            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
-            String dayName = dayFormat.format(date);
 
-            System.out.println("Day of the week: " + dayName);
-        } catch (Exception e) {
-            System.out.println("Invalid date format. Please use dd/MM/yyyy.");
-        }
 
-        scanner.close();
     }
 }
